762364652611382ce504ad25eb3216974f61b850
NirajLalwani <nirajlalwani2810@gmail.com>
2024-04-01 00:28:09 +0530

    added

--- services/baseurl.js
+++ services/baseurl.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = "https://e-commerce-backend-2-q99m.onrender.com"
-export const BASE_URL = "http://localhost:4000"
+export const BASE_URL = "https://e-commerce-backend-2-q99m.onrender.com"
+// export const BASE_URL = "http://localhost:4000"
