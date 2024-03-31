353f7519da259c4c8b6de56c71b196a31794a9a7
NirajLalwani <nirajlalwani2810@gmail.com>
2024-03-31 17:27:24 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -15,21 +15,21 @@ import SingleProduct from "./pages/SingleProduct"
 import AddToCart from './pages/AddToCart.jsx'
 import Products from "./pages/Products"
 import Footer from "./components/Footer"
-import {useHistory} from 'react-router-dom'
+// import {useHistory} from 'react-router-dom'
 
 function App() {
 
 
-  const history = useHistory();
+  // const history = useHistory();
 
-  useEffect(() => {
-    const unlisten = history.listen(() => {
-      window.scrollTo(0, 0);
-    });
-    return () => {
-      unlisten();
-    };
-  }, [history]);
+  // useEffect(() => {
+  //   const unlisten = history.listen(() => {
+  //     window.scrollTo(0, 0);
+  //   });
+  //   return () => {
+  //     unlisten();
+  //   };
+  // }, [history]);
 
 
   return (
