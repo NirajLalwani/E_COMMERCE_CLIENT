d7edf2c9abdd946d8316bbcbcd0eedf8a10a53b2
NirajLalwani <nirajlalwani2810@gmail.com>
2024-03-31 18:07:52 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React, {  } from "react"
 import { Route, Routes } from 'react-router-dom'
 import Register from "./pages/register"
 import './styles/App.css'
